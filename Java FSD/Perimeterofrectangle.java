de5e0f3762748aff599cb79c286bd4665ffa1c7d
Nitish1098 <98463564+Nitish1098@users.noreply.github.com>
2022-02-02 14:30:35 +0530

    Add files via upload

---
+++ Java FSD/Perimeterofrectangle.java
@@ -0,0 +1,19 @@
+package javademo;
+
+public class Perimeterofrectangle {
+
+	public static void main(String[] args) {
+	
+		{ int a,b,c,d,perimeter;
+		
+		a=c=5;
+		b=d=4;
+		
+		perimeter=2*(a+b);
+		
+		System.out.println("Perimeter of rectangle is" +perimeter);
+		}
+
+	}
+
+}
